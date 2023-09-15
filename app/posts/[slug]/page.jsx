61db0617738e15dfb6e92f16318c5ec56ac65ac6
emreates2 <emre.ates@trailblu.com>
2023-09-15 18:19:18 +0300

    post details and user

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -3,65 +3,67 @@ import Menu from "@/components/menu";
 import Image from "next/image";
 import React from "react";
 
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
+  console.log(data);
   return (
     <div className="">
-      <div className="block md:flex md:flex-row-reverse items-center gap-8">
-        <div className="relative my-4 h-56 flex-1 md:my-0 md:h-80">
-          <Image
-            src="/p1.jpeg"
-            alt=""
-            fill
-            className="rounded-3xl object-cover"
-            sizes="200px"
-          />
-        </div>
+      <div className="block items-center gap-8 md:flex md:flex-row-reverse">
+        {data.image && (
+          <div className="relative my-4 h-56 flex-1 md:my-0 md:h-80">
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              fill
+              className="rounded-3xl object-cover"
+              sizes="200px"
+            />
+          </div>
+        )}
 
         <div className="flex-1">
-          <h1 className="mb-4 text-2xl md:text-5xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Doloremque.
-          </h1>
+          <h1 className="mb-4 text-2xl md:text-5xl">{data.title}</h1>
           <div className="flex items-center gap-4">
-            <div className="relative h-12 w-12">
-              <Image
-                src="/p1.jpeg"
-                alt=""
-                fill
-                className="rounded-full object-cover"
-                sizes="50px"
-              />
-            </div>
+            {data.user.image && (
+              <div className="relative h-12 w-12">
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className="rounded-full object-cover"
+                  sizes="50px"
+                />
+              </div>
+            )}
             <div className="flex flex-col gap-1 text-gray-400">
-              <span className="text-lg">John Doe</span>
+              <span className="text-lg">{data.user.name}</span>
               <span>01.01.2023</span>
             </div>
           </div>
         </div>
       </div>
       <div className="flex gap-8">
-        <div className="mt-6 md:mt-16 flex-[5]">
-          <div className="space-y-4 text-xl font-light">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-              consequuntur quae esse enim omnis vero quia, quibusdam nemo
-              asperiores? Aliquid temporibus nobis cupiditate fugiat earum non
-              tempore dolores reiciendis corporis.
-            </p>
-            <h1>Lorem ipsum dolor sit amet.</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-              consequuntur quae esse enim omnis vero quia, quibusdam nemo
-              asperiores? Aliquid temporibus nobis cupiditate fugiat earum non
-              tempore dolores reiciendis corporis.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-              consequuntur quae esse enim omnis vero quia, quibusdam nemo
-              asperiores? Aliquid temporibus nobis cupiditate fugiat earum non
-              tempore dolores reiciendis corporis.
-            </p>
-          </div>
+        <div className="mt-6 flex-[5] md:mt-16">
+          <div
+            className="space-y-4 text-xl font-light"
+            dangerouslySetInnerHTML={{ __html: data?.description }}
+          />
+
           <div>
             <Comments />
           </div>
