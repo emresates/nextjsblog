4d06983360aab81ee4000ba8932d19782608db99
emreates2 <emre.ates@trailblu.com>
2023-09-18 14:15:22 +0300

    test13

--- utils/connect.js
+++ utils/connect.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/generated/client";
+
 let prisma;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
